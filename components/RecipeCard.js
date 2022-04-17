57483703544d9b1af0ff6b4a2393ecb60fd57199
antonis-angelis <agelisadonis@gmail.com>
2022-04-17 20:32:31 +0300

    render some information about recipes

--- components/RecipeCard.js
+++ components/RecipeCard.js
@@ -1,5 +1,24 @@
+import Link from "next/link";
+
 const RecipeCard = ({ recipe }) => {
-  return <div>{recipe.fields.title}</div>;
+  const { title, slug, cookingTime, thumbnail } = recipe.fields;
+
+  return (
+    <div className="card">
+      <div className="featured"></div>
+      <div className="content">
+        <div className="info">
+          <h4>{title}</h4>
+          <p>Takes approximately {cookingTime} minutes to make</p>
+        </div>
+        <div className="actions">
+          <Link href={`/recipes/${slug}`}>
+            <a>Cook this</a>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default RecipeCard;
